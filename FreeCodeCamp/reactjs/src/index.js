c03a35545616d35e8ff39d9bf1f415d09dcf8e54
Eduardo <edalvarez2015@gmail.com>
2021-01-22 19:52:35 -0600

    Hour 3 done - React Props

--- FreeCodeCamp/reactjs/src/index.js
+++ FreeCodeCamp/reactjs/src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const books = [
+  {
+    id: 1,
+    title: "I love you to the moon and Back",
+    author: "Amelia Hepworth",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61O3XxoArTL._AC_SY400_.jpg",
+  },
+  {
+    id: 2,
+    title: "I love you to the moon and Back",
+    author: "Amelia Hepworth",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61O3XxoArTL._AC_SY400_.jpg",
+  },
+  {
+    id: 3,
+    title: "Ender's Game",
+    author: "Orson Scott Card",
+    img: "https://m.media-amazon.com/images/I/81+IUsYtGTL._AC_UY218_.jpg",
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BookList = () => {
+  return (
+    <section className="bookList">
+      {books.map((book) => {
+        return <Book key={book.id} book={book}></Book>;
+      })}
+    </section>
+  );
+};
+
+const Book = (props) => {
+  const { img, title, author } = props.book;
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+ReactDOM.render(<BookList />, document.getElementById("root"));
